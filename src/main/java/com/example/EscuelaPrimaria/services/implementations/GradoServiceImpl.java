2430fd832ee95c0852a42ad99db33b35770c3990
Franco Lujan <francolujann1@gmail.com>
2025-08-27 20:04:36 -0300

    funcionamiento minimo de GradoServiceImpl

--- src/main/java/com/example/EscuelaPrimaria/services/implementations/GradoServiceImpl.java
+++ src/main/java/com/example/EscuelaPrimaria/services/implementations/GradoServiceImpl.java
@@ -1,18 +1,27 @@
 package com.example.EscuelaPrimaria.services.implementations;
 
+import com.example.EscuelaPrimaria.dtos.entrada.GradoDtoE;
+import com.example.EscuelaPrimaria.dtos.salida.GradoDtoS;
 import com.example.EscuelaPrimaria.entities.Grado;
+import com.example.EscuelaPrimaria.enums.NivelEnum;
 import com.example.EscuelaPrimaria.gestores.GestorRepo;
 import com.example.EscuelaPrimaria.services.interfaces.GradoService;
+import jakarta.persistence.EntityExistsException;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Service
 @AllArgsConstructor
-
 public class GradoServiceImpl implements GradoService<Grado, Long> {
 
 
@@ -37,16 +46,88 @@ public class GradoServiceImpl implements GradoService<Grado, Long> {
     }
 
     @Override
-    public List<Grado> getAll() {
+    public List<Grado> findAll() {
         return gestor.getGradoRepository().findAll();
     }
 
     @Override
-    public Grado getById(Long id) {
+    public Grado findById(Long id) {
         return gestor.getGradoRepository().findById(id)
                 .orElseThrow(() -> new EntityNotFoundException("no se encontro grado con el id:" + id));
     }
 
+    @Override
+    public boolean findGradoByNivelEqualsIgnoreCase(int nivel) {
+        return gestor.getGradoRepository().findGradoByNivelEqualsIgnoreCase(nivel);
+    }
+
+    @Override
+    public boolean findGradoByTurnoEqualsIgnoreCase(String turnoId) {
+        return gestor.getGradoRepository().findGradoByTurnoEqualsIgnoreCase(turnoId);
+    }
+
+    @Override
+    public List<Grado> findGradoByNivel(int nivel) {
+        return gestor.getGradoRepository().findGradoByNivel(nivel);
+    }
+
+
+    public void agregarGrado(GradoDtoE grado) {
+
+        if (!existenciaGrado(grado)) {
+            ModelMapper modelMapper = new ModelMapper();
+            Grado gradoEntity = modelMapper.map(grado, Grado.class);
+            gestor.getGradoRepository().save(gradoEntity);
+        } else {
+            throw new EntityExistsException("El grado existe en el sistema");
+        }
+
+    }
+
+    public void actualizarGrado(GradoDtoE grado) {
+
+        if (existenciaGrado(grado)) {
+            ModelMapper modelMapper = new ModelMapper();
+            Grado gradoEntity = modelMapper.map(grado, Grado.class);
+            gestor.getGradoRepository().save(gradoEntity);
+        } else {
+            throw new EntityNotFoundException("El grado no existe en el sistema");
+        }
+
+    }
+
+    public void eliminarGrado(GradoDtoE grado) {
+        if (existenciaGrado(grado)) {
+            ModelMapper modelMapper = new ModelMapper();
+            Grado gradoEntity = modelMapper.map(grado, Grado.class);
+            gestor.getGradoRepository().delete(gradoEntity);
+
+        } else {
+            throw new EntityNotFoundException("El grado no se puede eliminar porque no existe");
+        }
+
+
+    }
+
+    public List<GradoDtoS> todos() {
+        ModelMapper modelMapper = new ModelMapper();
+        List<Grado> grados = gestor.getGradoRepository().findAll();
+        return grados.stream().map(grado -> modelMapper.map(grado, GradoDtoS.class)).toList();
+    }
+
+    public List<GradoDtoS> buscarGradoPorNivel(int nivel) {
+        ModelMapper modelMapper = new ModelMapper();
+        List<Grado> grados = findGradoByNivel(nivel);
+        return grados.stream().map(grado -> modelMapper.map(grado, GradoDtoS.class)).toList();
+
+
+    }
+
+    private boolean existenciaGrado(GradoDtoE grado) {
+        //            throw new EntityExistsException("El grado existe en el sistema");
+        return findGradoByNivelEqualsIgnoreCase(grado.getNivel().getValor())
+                && findGradoByTurnoEqualsIgnoreCase(grado.getTurno().getNombre().name());
+    }
 
 
 }

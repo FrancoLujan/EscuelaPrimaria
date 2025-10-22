4986b1f17f71d6f27066b5b9c5fd81c3d51570fc
Franco Lujan <francolujann1@gmail.com>
2025-10-21 21:53:30 -0300

    refactorizacion de MateriaServiceImpl

--- src/main/java/com/example/EscuelaPrimaria/services/implementations/domain/MateriaServiceImpl.java
+++ src/main/java/com/example/EscuelaPrimaria/services/implementations/domain/MateriaServiceImpl.java
@@ -3,103 +3,110 @@ package com.example.EscuelaPrimaria.services.implementations.domain;
 import com.example.EscuelaPrimaria.dtos.entrada.MateriaDtoE;
 import com.example.EscuelaPrimaria.entities.domain.Materia;
 
-import com.example.EscuelaPrimaria.gestores.GestorRepositoryDomain;
+import com.example.EscuelaPrimaria.errors.MensajeErrorValidaciones;
+import com.example.EscuelaPrimaria.repositories.domain.MateriaRepository;
 import com.example.EscuelaPrimaria.services.interfaces.domain.MateriaService;
+import jakarta.persistence.EntityExistsException;
 import jakarta.persistence.EntityNotFoundException;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
 public class MateriaServiceImpl implements MateriaService<Materia, Long> {
-    private final GestorRepositoryDomain gestorRepo;
+    private final MateriaRepository repo;
 
 
     @Override
     public void add(Materia entity) {
-        gestorRepo.getMateriaRepository().save(entity);
+        repo.save(entity);
     }
 
     @Override
     public void update(Materia entity) {
-        gestorRepo.getMateriaRepository().save(entity);
+        repo.save(entity);
 
     }
 
     @Override
     public void delete(Long id) {
-        gestorRepo.getMateriaRepository().deleteById(id);
+        repo.deleteById(id);
 
     }
 
     @Override
     public List<Materia> findAll() {
-        return gestorRepo.getMateriaRepository().findAll();
+        return repo.findAll();
     }
 
     @Override
     public Materia findById(Long id) {
-        return gestorRepo.getMateriaRepository().findById(id).
+        return repo.findById(id).
                 orElseThrow(() -> new EntityNotFoundException("No se encontro Materia"));
     }
+
     @Override
     public Materia findMateriaByNombre(String nombre) {
-        return null;
+
+        return repo.findMateriaByNombre(nombre);
     }
 
     @Override
     // aplicaste la logica aca y no en un metodo privado porque no es tan complejo
     public boolean findExistsMateriaByNombre(String nombre) {
-        if(nombre != null && !nombre.isEmpty()){
-            return gestorRepo.getMateriaRepository().findMateriaByNombreEqualsIgnoreCase(nombre);
+            return repo.findMateriaByNombreEqualsIgnoreCase(nombre);
         }
-        return false;
 
-    }
 
-    public void agregarMateria(MateriaDtoE materiaDto) throws IllegalArgumentException {
-        if(materiaDto != null && !findExistsMateriaByNombre(materiaDto.getNombre())) {
-            ModelMapper modelMapper = new ModelMapper();
-            Materia materia = modelMapper.map(materiaDto, Materia.class);
-            gestorRepo.getMateriaRepository().save(materia);
-        }
-        throw new IllegalArgumentException("Argumentos vacios o duplicado");
 
-    }
 
-    public void eliminarMateria(MateriaDtoE materiaDto) throws IllegalArgumentException {
-        if(materiaDto != null) {
-            delete(getMateriaByNombre(materiaDto.getNombre()).getId());
-        }
-        throw new IllegalArgumentException("No se puede eliminar la materia");
+    public void agregarMateria(@NotNull @NotBlank(message = MensajeErrorValidaciones.MENSAJE_NOMBRE) String materiaNueva) throws EntityExistsException {
 
-    }
-    public void actualizarMateria(MateriaDtoE materiaDto) throws Exception {
-        if(materiaDto != null && findExistsMateriaByNombre(materiaDto.getNombre())) {
+        if (findExistsMateriaByNombre(materiaNueva)) {
             Materia materia = new Materia();
-            materia.setNombre(materiaDto.getNombre());
+            materia.setNombre(materiaNueva);
+            add(materia);
+        }
+        else {
+            throw new EntityExistsException("La entidad ya existe ");
+        }
+
+    }
+
+    public void eliminarMateria(@NotNull @NotBlank(message = MensajeErrorValidaciones.MENSAJE_NOMBRE) String materia) throws EntityNotFoundException {
+        if (findExistsMateriaByNombre(materia)) {
+            delete(findMateriaByNombre(materia).getId());
+        }
+        else {
+            throw new EntityNotFoundException("No existe la materia");
+        }
+
+
+    }
+
+    public void actualizarMateria(String materiaActualizar) throws EntityNotFoundException {
+        if (findExistsMateriaByNombre(materiaActualizar)) {
+            Materia materia = new Materia();
+            materia.setNombre(materiaActualizar);
             update(materia);
         }
-        throw new Exception("No se puede actualizar la materia");
-    }
-
-    //Se usa MateriaDtoE porque solo tiene el atributo nombre
-    public List<MateriaDtoE> findAllMateriasPorGrado() {
-        ModelMapper modelMapper = new ModelMapper();
-        List<Materia> materias = gestorRepo.getMateriaRepository().findAll();
-        return materias.stream().map(e -> modelMapper.map(e, MateriaDtoE.class)).toList();
-    }
-
-    private Materia getMateriaByNombre(String nombre)throws EntityNotFoundException{
-        if(nombre != null && findExistsMateriaByNombre(nombre)) {
-            return findMateriaByNombre(nombre);
+        else{
+            throw new EntityNotFoundException("No se puede actualizar la materia");
         }
-        throw new EntityNotFoundException("No se encontro la materia: " + nombre);
 
     }
+
+    public List<String> todos() {
+        List<Materia> materias = repo.findAll();
+        return materias.stream().map(Materia::getNombre).toList();
+    }
+
 
 
 }

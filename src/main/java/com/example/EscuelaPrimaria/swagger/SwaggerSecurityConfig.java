210ce1e67c021a83d39b97c05caf7a463724774e
Franco Lujan <francolujann1@gmail.com>
2025-11-29 20:56:05 -0300

    Configuracion de swagger y documentacion para equipo de front

--- src/main/java/com/example/EscuelaPrimaria/swagger/SwaggerSecurityConfig.java
+++ src/main/java/com/example/EscuelaPrimaria/swagger/SwaggerSecurityConfig.java
@@ -4,6 +4,9 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -16,4 +19,17 @@ import org.springframework.context.annotation.Configuration;
         scheme = "basic"
 )
 public class SwaggerSecurityConfig {
+
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(
+                        new Info()
+                                .title("Api Escuela Primaria")
+                                .version("1.0")
+                                .description("Documentacion de api")
+                );
+    }
+
 }

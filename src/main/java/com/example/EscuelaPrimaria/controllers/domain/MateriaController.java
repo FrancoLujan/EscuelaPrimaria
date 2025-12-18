773c54879f1ff884474040e2e1a7846e14f56f7a
Franco Lujan <francolujann1@gmail.com>
2025-12-18 13:44:45 -0300

    controlador finalzado asi como el crud refinado de materia

--- src/main/java/com/example/EscuelaPrimaria/controllers/domain/MateriaController.java
+++ src/main/java/com/example/EscuelaPrimaria/controllers/domain/MateriaController.java
@@ -1,9 +1,63 @@
 package com.example.EscuelaPrimaria.controllers.domain;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.EscuelaPrimaria.dtos.entrada.MateriaDtoE;
+import com.example.EscuelaPrimaria.services.implementations.domain.MateriaServiceImpl;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/Materia")
+@AllArgsConstructor
+@PreAuthorize("permitAll()")
 public class MateriaController {
+    final private MateriaServiceImpl servicio;
+
+    @GetMapping("/todos")
+    public ResponseEntity<List<String>> todos(){
+        List<String> materias = servicio.todos();
+        return ResponseEntity.status(HttpStatus.OK).body(materias);
+    }
+
+    @GetMapping("/todos/{nivel}")
+    public ResponseEntity<List<String>> todos(@PathVariable Long nivel){
+        List<String> materias = servicio.todos(nivel);
+        return ResponseEntity.status(HttpStatus.OK).body(materias);
+
+    }
+
+
+    @GetMapping("/materia/{nombre}")
+    public ResponseEntity<String> buscarMateriaPorNombre(@PathVariable String nombre){
+        String materia = servicio.buscarMateriaPorNombre(nombre);
+        return ResponseEntity.status(HttpStatus.OK).body(materia);
+    }
+
+    @PostMapping("/crear")
+    public ResponseEntity<String> crearMateria(@RequestBody MateriaDtoE dto){
+        servicio.agregarMateria(dto.getNombre());
+        return ResponseEntity.status(HttpStatus.CREATED).body(dto.getNombre());
+    }
+
+    @PatchMapping("/actualizar/{nombreMateria}")
+    public ResponseEntity<String> actualizarMateria(@RequestBody MateriaDtoE dto, @PathVariable String nombreMateria){
+        servicio.actualizarMateria(nombreMateria, dto.getNombre());
+        return ResponseEntity.status(HttpStatus.OK).body("matteria actualizada");
+    }
+
+    @DeleteMapping("/eliminar/{nombreMateria}")
+    public ResponseEntity<String> eliminarMateria(@PathVariable String nombreMateria){
+        servicio.eliminarMateria(nombreMateria);
+        return ResponseEntity.status(HttpStatus.OK).body("Se elimino la materia");
+    }
+
+
+
+
+
+
 }
--- src/main/java/com/example/EscuelaPrimaria/services/implementations/domain/MateriaServiceImpl.java
+++ src/main/java/com/example/EscuelaPrimaria/services/implementations/domain/MateriaServiceImpl.java
@@ -1,6 +1,8 @@
 package com.example.EscuelaPrimaria.services.implementations.domain;
 
 import com.example.EscuelaPrimaria.dtos.entrada.MateriaDtoE;
+import com.example.EscuelaPrimaria.dtos.salida.MateriaDtoS;
+import com.example.EscuelaPrimaria.entities.domain.Grado;
 import com.example.EscuelaPrimaria.entities.domain.Materia;
 
 import com.example.EscuelaPrimaria.errors.MensajeErrorValidaciones;
@@ -60,52 +62,84 @@ public class MateriaServiceImpl implements MateriaService<Materia, Long> {
     @Override
     // aplicaste la logica aca y no en un metodo privado porque no es tan complejo
     public boolean findExistsMateriaByNombre(String nombre) {
-            return repo.findMateriaByNombreEqualsIgnoreCase(nombre);
-        }
+        return repo.findMateriaByNombreEqualsIgnoreCase(nombre);
+    }
 
+    @Override
+    public List<Materia> findMateriaByGrado_Id(Long id) {
+        return repo.findMateriaByGrado_Id(id);
+    }
 
-
+    @Override
+    public List<Materia> findMateriaByGrado_Nivel(Long nivel) {
+        return repo.findMateriaByGrado_Nivel(nivel);
+    }
 
     public void agregarMateria(@NotNull @NotBlank(message = MensajeErrorValidaciones.MENSAJE_NOMBRE) String materiaNueva) throws EntityExistsException {
 
-        if (findExistsMateriaByNombre(materiaNueva)) {
+        if (findMateriaByNombre(materiaNueva) == null) {
             Materia materia = new Materia();
             materia.setNombre(materiaNueva);
             add(materia);
-        }
-        else {
+        } else {
             throw new EntityExistsException("La entidad ya existe ");
         }
 
     }
 
     public void eliminarMateria(@NotNull @NotBlank(message = MensajeErrorValidaciones.MENSAJE_NOMBRE) String materia) throws EntityNotFoundException {
-        if (findExistsMateriaByNombre(materia)) {
+        if (findMateriaByNombre(materia) != null) {
             delete(findMateriaByNombre(materia).getId());
-        }
-        else {
+        } else {
             throw new EntityNotFoundException("No existe la materia");
         }
 
 
     }
 
-    public void actualizarMateria(String materiaActualizar) throws EntityNotFoundException {
-        if (findExistsMateriaByNombre(materiaActualizar)) {
-            Materia materia = new Materia();
-            materia.setNombre(materiaActualizar);
+    public void actualizarMateria(@NotNull @NotBlank(message = MensajeErrorValidaciones.MENSAJE_NOMBRE) String materiaActualizar,
+                                  @NotNull @NotBlank(message = MensajeErrorValidaciones.MENSAJE_NOMBRE) String materiaNueva  ) throws EntityNotFoundException {
+        if (findMateriaByNombre(materiaActualizar) != null) {
+            Materia materia = findMateriaByNombre(materiaActualizar);
+            materia.setNombre(materiaNueva);
             update(materia);
-        }
-        else{
+        } else {
             throw new EntityNotFoundException("No se puede actualizar la materia");
         }
 
     }
 
-    public List<String> todos() {
+    // metodos para ver nombres de materias Casos especificos
+    public List<String> todos() throws EntityNotFoundException {
         List<Materia> materias = repo.findAll();
+        if (materias.isEmpty()) {
+            throw new EntityNotFoundException("No existe las materias");
+        }
         return materias.stream().map(Materia::getNombre).toList();
     }
+
+    public String buscarMateriaPorNombre(@NotNull @NotBlank(message = MensajeErrorValidaciones.MENSAJE_NOMBRE) String nombre) throws EntityNotFoundException {
+        Materia materia = repo.findMateriaByNombre(nombre);
+        if (materia == null) {
+            throw new EntityNotFoundException("No existe la materia");
+        }
+        return materia.getNombre();
+    }
+    public List<String> todos(@NotNull @NotBlank(message = MensajeErrorValidaciones.MENSAJE_NUMERO) Long nivel)throws  EntityNotFoundException{
+        List<Materia> materias = repo.findMateriaByGrado_Nivel(nivel);
+
+        if (materias.isEmpty()) {
+            throw new EntityNotFoundException("No existen materias en ese grado");
+        }
+        return materias.stream().map(Materia::getNombre).toList();
+
+    }
+
+    /**
+     la asignacion de notas se hara en un servicio aparte para evitar exceso  la clase es VidaEstudiantil
+     */
+
+
 
 
 

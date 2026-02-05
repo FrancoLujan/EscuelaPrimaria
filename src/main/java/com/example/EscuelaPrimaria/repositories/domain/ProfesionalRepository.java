4020f9cf4661d53f4d70bf1c42ad40fa72ee1a82
Franco Lujan <francolujann1@gmail.com>
2026-02-05 18:15:29 -0300

    repositorio listo

--- src/main/java/com/example/EscuelaPrimaria/repositories/domain/ProfesionalRepository.java
+++ src/main/java/com/example/EscuelaPrimaria/repositories/domain/ProfesionalRepository.java
@@ -4,6 +4,11 @@ import com.example.EscuelaPrimaria.entities.domain.Profesional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProfesionalRepository extends JpaRepository<Profesional, Long> {
+    List<Profesional> findProfesionalByCuil(Long cuil);
+    Profesional findProfesionalByUsuario_Id(Long idUsuario);
+
 }

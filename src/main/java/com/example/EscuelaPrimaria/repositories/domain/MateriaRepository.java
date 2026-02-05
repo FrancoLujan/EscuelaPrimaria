4aa72c92e9f879f930ca772b4c53e1b9712f4c92
Franco Lujan <francolujann1@gmail.com>
2026-02-05 18:19:47 -0300

    se hicieron pequeñas modificaciones

--- src/main/java/com/example/EscuelaPrimaria/repositories/domain/MateriaRepository.java
+++ src/main/java/com/example/EscuelaPrimaria/repositories/domain/MateriaRepository.java
@@ -1,8 +1,11 @@
 package com.example.EscuelaPrimaria.repositories.domain;
 
+import com.example.EscuelaPrimaria.entities.domain.Grado;
 import com.example.EscuelaPrimaria.entities.domain.Materia;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface MateriaRepository extends JpaRepository<Materia, Long> {

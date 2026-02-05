8600d16f9b629369da7b754eb4991f44250cd9f8
Franco Lujan <francolujann1@gmail.com>
2026-02-05 18:07:02 -0300

    se cambio sus tipo

--- src/main/java/com/example/EscuelaPrimaria/dtos/salida/ProfesionalDtoS.java
+++ src/main/java/com/example/EscuelaPrimaria/dtos/salida/ProfesionalDtoS.java
@@ -6,6 +6,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter @Setter

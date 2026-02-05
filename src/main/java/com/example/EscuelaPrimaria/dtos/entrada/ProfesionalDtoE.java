f4e7c2e9ba98a85c40a75ae6b621c15de11f19df
Franco Lujan <francolujann1@gmail.com>
2026-02-05 18:20:53 -0300

    Se mejoro las validaciones

--- src/main/java/com/example/EscuelaPrimaria/dtos/entrada/ProfesionalDtoE.java
+++ src/main/java/com/example/EscuelaPrimaria/dtos/entrada/ProfesionalDtoE.java
@@ -1,5 +1,10 @@
 package com.example.EscuelaPrimaria.dtos.entrada;
 
+import com.example.EscuelaPrimaria.errors.MensajeErrorValidaciones;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,8 +13,15 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Getter
 public class ProfesionalDtoE {
+    @NotEmpty(message = MensajeErrorValidaciones.MENSAJE_NOMBRE)
     private String nombre;
+
+    @NotEmpty(message = MensajeErrorValidaciones.MENSAJE_NOMBRE)
     private String apellido;
+
+    @NotNull(message = MensajeErrorValidaciones.MENSAJE_NUMERO)
+    @Min(value = 99999999, message = MensajeErrorValidaciones.MENSAJE_NUMERO)
+    @Max(value = 9999999999L ,message = MensajeErrorValidaciones.MENSAJE_NUMERO)
     private Long cuil;
 
 
